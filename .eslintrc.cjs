32fc814ca95e1b1e7563067c6fc39229255aeb28
RazorYui <dezx97@gmail.com>
2023-01-22 11:13:14 +0200

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,15 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
+  rules: {
+    'vue/multi-word-component-names': 'off', 
+  }
 };
